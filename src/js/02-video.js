a1a5487ca6b4eb4532ecf731225d9a776bcb9b96
krok-code <maks.smolinskyi@gmail.com>
2023-11-19 23:01:02 +0200

    wrote the code for all four tasks

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,21 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.getElementById('vimeo-player');
+const player = new Player(iframe);
+
+document.addEventListener('DOMContentLoaded', () => {
+    const savedTime = localStorage.getItem('videoplayer-current-time');
+    if (savedTime) {
+        player.setCurrentTime(savedTime);
+    }
+});
+
+const saveTimeThrottled = throttle((currentTime) => {
+    localStorage.setItem('videoplayer-current-time', currentTime);
+}, 1000);
+
+player.on('timeupdate', (data) => {
+    const currentTime = data.seconds;
+    saveTimeThrottled(currentTime);
+});
